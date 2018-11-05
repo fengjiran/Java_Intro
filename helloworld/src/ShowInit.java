9655a4e062edcebfcfb1dbef2bb4fc0b2abacd85
fengjiran <fengjiran@foxmail.com>
2018-11-05 18:15:05 +0800

    yes

--- helloworld/src/ShowInit.java
+++ helloworld/src/ShowInit.java
@@ -1,2 +1,8 @@
 public class ShowInit {
+    private static int a;
+    private static char str;
+    public static void main(String[] args){
+        System.out.println(a);
+        System.out.println(str);
+    }
 }
