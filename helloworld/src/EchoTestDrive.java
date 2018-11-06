f231c6793f80f0da8cd9c0714cd7c10580c273e4
fengjiran <fengjiran@foxmail.com>
2018-11-06 11:27:59 +0800

    comment a line

--- helloworld/src/EchoTestDrive.java
+++ helloworld/src/EchoTestDrive.java
@@ -1,8 +1,8 @@
 public class EchoTestDrive {
     public static void main(String[] args) {
         Echo e1 = new Echo();
-        Echo e2 = e1;
-//        Echo e2 = new Echo();
+//        Echo e2 = e1;
+        Echo e2 = new Echo();
 
         int x = 0;
         while (x < 4) {
