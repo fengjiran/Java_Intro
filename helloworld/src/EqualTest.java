5d1e8c6a763bc30d66783587ae3b49c07d05883d
fengjiran <fengjiran@foxmail.com>
2018-11-07 10:19:08 +0800

    done

---
+++ helloworld/src/EqualTest.java
@@ -0,0 +1,31 @@
+public class EqualTest {
+    public static void main(String[] args){
+        Dog spot = new Dog();
+        Dog scruffy = new Dog();
+
+        spot.name = "spot";
+        scruffy.name = "scruffy";
+
+        spot.says = "Ruff!";
+        scruffy.says = "Wurf!";
+
+        System.out.println(spot.says);
+        System.out.println(scruffy.says);
+
+        Dog ric = new Dog();
+        ric = spot;
+
+        System.out.println(spot == scruffy);
+        System.out.println(spot == ric);
+        System.out.println(ric == scruffy);
+
+        System.out.println(spot.equals(scruffy));
+        System.out.println(spot.equals(ric));
+        System.out.println(ric.equals(scruffy));
+    }
+}
+
+class Dog {
+    String name;
+    String says;
+}
