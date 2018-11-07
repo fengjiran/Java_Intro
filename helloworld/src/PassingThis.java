0d123fcba37ec8c9cdf6522f06337ba701b19093
fengjiran <fengjiran@foxmail.com>
2018-11-07 16:27:44 +0800

    done

--- helloworld/src/PassingThis.java
+++ helloworld/src/PassingThis.java
@@ -1,2 +1,24 @@
 public class PassingThis {
+    public static void main(String[] args) {
+        new Person().eat(new Apple());
+    }
+}
+
+class Peeler {
+    static Apple peel(Apple apple) {
+        return apple;
+    }
+}
+
+class Apple {
+    Apple getPeeled (){
+        return Peeler.peel(this);
+    }
+}
+
+class Person {
+    public void eat(Apple apple) {
+        Apple peeled = apple.getPeeled();
+        System.out.println("Yummy");
+    }
 }
