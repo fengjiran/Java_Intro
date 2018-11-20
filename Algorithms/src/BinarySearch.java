5ab3675f94fbdbabaf00f0578101834f0ebc6ae7
fengjiran <fengjiran@foxmail.com>
2018-11-20 16:26:50 +0800

    rank first

--- Algorithms/src/BinarySearch.java
+++ Algorithms/src/BinarySearch.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 
 public class BinarySearch {
-    // 一般情况下的二分查找
+    // 假设数组是递增序列
+    // 一般情况下的二分查找，经典的二分查找
     // 没有考虑数组中具有多个重复的key的情况
     public static int rank(int key, int[] a) {
         int lo = 0;
@@ -19,11 +20,25 @@ public class BinarySearch {
         return -1;
     }
 
-    //寻找重复key中第一个出现的位置
+    // 假设数组是递增序列
+    // 寻找重复key中第一个出现的位置
     public static int rankFirst(int key, int[] a) {
         int l = 0;
         int h = a.length - 1;
         int m;
+        while (l <= h) {
+            m = l + (h - l)/2;
+            if (key < a[m])
+                h = m - 1;
+            else if (key > a[m])
+                l = m + 1;
+            else {
+                while (m >= 0 && key == a[m])
+                    m--;
+                m++;
+                return m;
+            }
+        }
 
         return -1;
     }
