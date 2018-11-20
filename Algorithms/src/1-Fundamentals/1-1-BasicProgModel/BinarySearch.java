8ce5208d204eda0b99366d9ff0467f279b2ff47a
fengjiran <fengjiran@foxmail.com>
2018-11-20 22:54:47 +0800

    binary search

---
+++ Algorithms/src/1-Fundamentals/1-1-BasicProgModel/BinarySearch.java
@@ -0,0 +1,112 @@
+import java.util.Arrays;
+
+public class BinarySearch {
+    // 假设数组是非递减序列
+    // 一般情况下的二分查找，经典的二分查找
+    // 没有考虑数组中具有多个重复的key的情况
+    public static int rank(int key, final int[] a) {
+        // 判断数组是否为空
+        if (a == null || a.length == 0)
+            return -1;
+
+        int lo = 0;
+        int hi = a.length - 1;
+        int mid;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;  // 直接平均可能会溢出
+            if (key < a[mid])
+                hi = mid - 1;
+            else if (key > a[mid])
+                lo = mid + 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+    // 假设数组是非递减序列
+    // 寻找重复key中第一个出现的位置
+    public static int rankFirst(int key, final int[] a) {
+        // 判断数组是否为空
+        if (a == null || a.length == 0)
+            return -1;
+
+        int l = 0;
+        int h = a.length - 1;
+        int m;
+        while (l <= h) {
+            m = l + (h - l) / 2;
+            if (key < a[m])
+                h = m - 1;
+            else if (key > a[m])
+                l = m + 1;
+            else {
+                while (m >= 0 && key == a[m])
+                    m--;
+                m++;
+                return m;
+            }
+        }
+
+        return -1;
+    }
+
+    // 假设数组是非递减序列
+    // 寻找重复key中最后一个出现的位置
+    public static int rankLast(int key, final int[] a) {
+        // 判断数组是否为空
+        if (a == null || a.length == 0)
+            return -1;
+
+        int l = 0;
+        int h = a.length - 1;
+        int m;
+        while (l <= h) {
+            m = l + (h - l) / 2;
+            if (key < a[m])
+                h = m - 1;
+            else if (key > a[m])
+                l = m + 1;
+            else {
+                while (m <= a.length - 1 && key == a[m])
+                    m++;
+                m--;
+                return m;
+            }
+        }
+        return -1;
+    }
+
+    public static int rankFirstII(int key, final int[] a) {
+        if (a == null || a.length == 0)
+            return -1;
+
+        int l = 0;
+        int h = a.length - 1;
+        int m;
+        while (l <= h) {
+            if (l == h)
+                return l;
+            m = l + (h - l) / 2;
+            if (key <= a[m])
+                h = m;
+            else
+                l = m + 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 3, 3, 3, 4, 5};
+        int loc1 = rank(3, a);
+        int loc2 = rankFirst(3, a);
+        int loc3 = rankLast(3, a);
+        int loc4 = rankFirstII(3, a);
+        System.out.println(loc1);
+        System.out.println(loc2);
+        System.out.println(loc3);
+        System.out.println(loc4);
+
+    }
+}
---
+++ HeadFirstJava/src/DrumKitTestDrive.java
@@ -0,0 +1,25 @@
+public class DrumKitTestDrive {
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.snare = false;
+        d.playTopHat();
+
+        if (d.snare)
+            d.playSnare();
+
+    }
+}
+
+class DrumKit {
+    boolean snare = true;
+    boolean topHat = true;
+
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+}
