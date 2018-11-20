8b2a6fc932ad6491a0b884e7f65ea40b3ee69cdd
fengjiran <fengjiran@foxmail.com>
2018-11-20 12:27:12 +0800

    prevent overflow

--- Algorithms/src/BinarySearch.java
+++ Algorithms/src/BinarySearch.java
@@ -5,7 +5,8 @@ public class BinarySearch {
         int lo = 0;
         int hi = a.length - 1;
         while (lo <= hi) {
-            int mid = (lo + hi) / 2;
+//            int mid = (lo + hi) / 2;
+            int mid = lo + (hi - lo) / 2;
             if (key < a[mid])
                 hi = mid - 1;
             else if (key > a[mid])
